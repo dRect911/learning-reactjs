2e370d6355c5a10eabf2c766eeec5019e433f0f8
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-05-22 12:40:00 +0000

    rendered a whole page from a page var

--- static-shit/index.js
+++ static-shit/index.js
@@ -36,6 +36,20 @@ const navbar = (
 )
 */
 
-ReactDOM.render(
-    navbar, 
-    document.getElementById("root"))
+const page =(
+    <div>
+        <img src="./react-logo-text.png" width="150px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has wll over 100K satrs on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+
+
+ReactDOM.render(page, document.getElementById("root"))
