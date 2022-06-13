77218d4ae2ffe0d602e9e37a467aeef60e135fe4
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-06-13 22:00:11 +0000

    added a null setup case (one-liners)

--- joke-machine/src/components/Joke.jsx
+++ joke-machine/src/components/Joke.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 
 
 export default function Joke(props){
-    return(
-        <div className="joke">
-            <p className="setup"></p>
-
-        </div>
-    )
+    if (props.setup == null){
+        return(
+            <div className="joke">
+                <span className="joke--punch">{props.punch}</span>
+            </div>
+        )
+    }
+    else{
+        return(
+            <div className="joke">
+                <span className="joke--setup">{props.setup}</span><br/>
+                <span className="joke--punch">{props.punch}</span>
+            </div>
+        )
+    }
+    
 }
