f6c77e5a21ec6c40d82f857cb3c16d9fef7c89f8
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-06-18 19:21:18 +0000

    updated with use of .map() method

--- joke-machine/src/components/Joke.jsx
+++ joke-machine/src/components/Joke.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 
+
 export default function Joke(props){
+
     if (props.setup == null){
         return(
             <div className="joke">
@@ -18,4 +20,18 @@ export default function Joke(props){
         )
     }
     
+    /* return(
+        <div className="joke">
+            {props.setup && <span className="joke--setup">{props.setup}</span>}<br/>
+            <span className="joke--punch">{props.punch}</span>
+        </div>
+    ) */
+
+    /* return(
+        <div className="joke">
+            <span className="joke--setup" style={{display: props.setup ? 'block' : 'none'}}>{props.setup}</span><br/>
+            <span className="joke--punch">{props.punch}</span>
+        </div>
+    ) */
+
 }
---
+++ joke-machine/src/components/JokesData.js
@@ -0,0 +1,69 @@
+export default[
+
+    {
+        setup:"How did the hacker escape the police?",
+        punch:"He just ransomware!"
+    },
+    {
+        setup:"I got my daughter a fridge for her birthday.",
+        punch:"I can't wait to see her face light up when she opens it."
+    },
+    {
+        setup:"Why don't pirates travel on mountain roads?",
+        punch:"Scurvy"
+    },
+    {
+        setup:"Why do bees stay in the hive in the winter?",
+        punch:"Swarm."
+    },
+    {
+        setup:"What's the best thing about Switzerland?",
+        punch:"I don't know, but the flag is a big plus!"
+    },
+    {
+        setup:"",
+        punch:"It’s hard to explain puns to kleptomaniacs because they always take things literally."
+    },
+    {
+        setup:"I went to a street where the houses were numbered 8k, 16k, 32k, 64k, 128k, 256k and 512k.",
+        punch:"It was a trip down Memory Lane."
+    },
+    {
+        setup:"Why do programmers always mix up Christmas and Halloween?",
+        punch:"Because Dec 25 is Oct 31."
+    },
+    {
+        setup:"Why do Java programmers have to wear glasses?",
+        punch:"Because they don’t C#."
+    },
+    {
+        setup:"",
+        punch:"If you listen to a UNIX shell, can you hear the C?"
+    },
+    {
+        setup:"What did the router say to the doctor?",
+        punch:"“It hurts when IP”"
+    },
+    {
+        setup:"",
+        punch:"“Debugging” is like being the detective in a crime drama where you are also the murderer."
+    },
+    {
+        setup:"//be nice to the CPU",
+        punch:"Thread_sleep(1);"
+    },
+    {
+        setup:"",
+        punch:"The best thing about a Boolean is that even if you are wrong, you are only off by a bit."
+    },
+    {
+        setup:"",
+        punch:"When Apple employees die, does their life HTML5 in front of their eyes?"
+    },
+
+    /* {
+        setup:,
+        punch:
+    }, */
+
+]
