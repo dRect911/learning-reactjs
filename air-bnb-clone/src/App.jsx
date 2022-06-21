e0817386f28ea02db64acf19573f83ef4deb962a
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-06-21 22:38:05 +0000

    use of map to render CArd component from a data file

--- air-bnb-clone/src/App.jsx
+++ air-bnb-clone/src/App.jsx
@@ -4,46 +4,30 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from './components/data'
 
 function App() {
-//<Hero />
+
+  const cards = data.map(card => {
+    return <Card 
+      img={card.img} 
+      rate={card.rate} 
+      reviews={card.reviews} 
+      location={card.location} 
+      title={card.title} 
+      cost={card.cost} 
+      openSpots={card.openSpots} 
+rate/>
+  });
+  
   return (
     <div className="App">
       <Navbar />
       <Hero />
       <div className="cards">
-        <Card 
-        img='"./img/img_no_radius.png"'
-        rate="5.0"
-        reviews="6"
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        cost="136"
-        />
-
-        <Card 
-        img='"./img/img_no_radius.png"'
-        rate="5.0"
-        reviews="30"
-        country="USA"
-        title="Learn wedding photography"
-        cost="125"
-        />
-
-        <Card 
-        img='"./img/img_no_radius.png"'
-        rate="4.5"
-        reviews="2"
-        country="USA"
-        title="Group Mountain Biking"
-        cost="50"
-        />
-
-
-
-        
+        {cards}
       </div>
-      
+      <Hero />
     </div>
   )
 }
--- air-bnb-clone/src/components/Card.jsx
+++ air-bnb-clone/src/components/Card.jsx
@@ -5,16 +5,18 @@ import Star from "../img/Star 1.png"
 export default function Card(props){
     return(
         <div className="card">
-            <img className="card--img" src={CardImage}></img>
-            <div className="card--desc">
+            <div className="card--badge">SOLD OUT</div>
+            <img className="card--img" src={props.img}></img>
+            <div className="card--stats">
                 <img className="card--star" src={Star}></img>
                 <span className="card--rate"> {props.rate} </span>
                 <span className="card--reviews"> ({props.reviews}) •</span>
-                <span className="card--country"> {props.country}</span><br/>
-                <span className="card--title">{props.title}</span><br/>
-                <span className="card--cost"><b>From ${props.cost}</b></span> / person
-            </div><br/>
-            
+                <span className="card--location"> {props.location}</span><br/>
+            </div>
+            <span className="card--title">{props.title}</span><br/>
+            <span className="card--cost"><b>From ${props.cost}</b></span> / person
+            <br/>
+        
         </div>
     )
 }
