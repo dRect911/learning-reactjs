bf607c01586d1e86771b7624236440f999f302e9
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-06-21 22:39:11 +0000

    a data file to be renderd as Card component

---
+++ air-bnb-clone/src/components/data.js
@@ -0,0 +1,31 @@
+export default[
+
+    {
+        img:'./src/img/img_no_radius.png',
+        rate:"5.0",
+        reviews:6,
+        location:"USA",
+        title:"Life lessons with Katie Zaferes",
+        cost:136,
+        openSpots: 0
+    },
+    {
+        img:'./src/img/wedding.png',
+        rate:"5.0",
+        reviews:30,
+        location:"USA",
+        title:"Learn wedding photography",
+        cost:125,
+        openSpots: 5
+    },
+    {
+        img:'./src/img/bike.png',
+        rate:"4.5",
+        reviews:2,
+        location:"USA",
+        title:"Group Mountain Biking",
+        cost:50,
+        openSpots: 12
+    },
+
+];
