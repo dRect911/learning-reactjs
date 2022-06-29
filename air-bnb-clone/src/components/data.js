9704143d2b1f07cd10597f4f66c678d8f2b3fc4b
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-06-29 08:53:17 +0000

    added location, open spots and id props

--- air-bnb-clone/src/components/data.js
+++ air-bnb-clone/src/components/data.js
@@ -1,24 +1,27 @@
 export default[
 
     {
+        id: 1,
         img:'./src/img/img_no_radius.png',
         rate:"5.0",
         reviews:6,
-        location:"USA",
+        location:"Online",
         title:"Life lessons with Katie Zaferes",
         cost:136,
         openSpots: 0
     },
     {
+        id: 2,
         img:'./src/img/wedding.png',
         rate:"5.0",
         reviews:30,
-        location:"USA",
+        location:"Online",
         title:"Learn wedding photography",
         cost:125,
         openSpots: 5
     },
     {
+        id: 3,
         img:'./src/img/bike.png',
         rate:"4.5",
         reviews:2,
