7f37e94d2c620c22cc9e68de82b505dfc216317e
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-06-29 08:54:04 +0000

    conditional render of a badge

--- air-bnb-clone/src/components/Card.jsx
+++ air-bnb-clone/src/components/Card.jsx
@@ -3,19 +3,28 @@ import CardImage from "../img/img_no_radius.png"
 import Star from "../img/Star 1.png"
 
 export default function Card(props){
+
+    let badgeText
+    if (props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    }
+    else if (props.location === "Online"){
+        badgeText = "ONLINE"
+    }
+
     return(
         <div className="card">
-            <div className="card--badge">SOLD OUT</div>
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img className="card--img" src={props.img}></img>
             <div className="card--stats">
                 <img className="card--star" src={Star}></img>
                 <span className="card--rate"> {props.rate} </span>
-                <span className="card--reviews"> ({props.reviews}) •</span>
+                <span className="card--reviews"> ({props.reviews}) • </span>
                 <span className="card--location"> {props.location}</span><br/>
             </div>
-            <span className="card--title">{props.title}</span><br/>
+            <span className="card--title">{props.title}</span><br/><br/>
             <span className="card--cost"><b>From ${props.cost}</b></span> / person
-            <br/>
+            
         
         </div>
     )
