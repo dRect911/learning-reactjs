aa5c043e3d3474dac5a5626b2102826d053a38bf
dRect911 <94997838+dRect911@users.noreply.github.com>
2022-06-30 20:29:52 +0000

    some tweaks

--- air-bnb-clone/src/components/data.js
+++ air-bnb-clone/src/components/data.js
@@ -30,5 +30,46 @@ export default[
         cost:50,
         openSpots: 12
     },
+    {
+        id: 4,
+        img:'https://picsum.photos/200/300?random=1',
+        rate:"4.5",
+        reviews:2,
+        location:"USA",
+        title:"Group Mountain Biking",
+        cost:30,
+        openSpots: 12
+    },
+    {
+        id: 5,
+        img:'https://picsum.photos/200/300?random=2',
+        rate:"3.0",
+        reviews:2,
+        location:"USA",
+        title:"Group Mountain Biking",
+        cost:150,
+        openSpots: 12
+    },
+    {
+        id: 6,
+        img:'https://picsum.photos/200/300?random=3',
+        rate:"2.5",
+        reviews:2,
+        location:"Online",
+        title:"Shit",
+        cost:50,
+        openSpots: 0
+    },
+    {
+        id: 6,
+        img:'https://picsum.photos/200/300?random=4',
+        rate:"4.7",
+        reviews:2,
+        location:"Online",
+        title:"More shit",
+        cost:750,
+        openSpots: 4
+    },
 
 ];
+
