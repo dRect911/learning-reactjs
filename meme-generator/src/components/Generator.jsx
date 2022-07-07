686b27c4a81f311a55c3aeeae0854ff79631464f
dRect911 <n3m0ken@gmail.com>
2022-07-07 08:05:22 +0000

    first use of state

--- meme-generator/src/components/Generator.jsx
+++ meme-generator/src/components/Generator.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import Troll from '../img/troll_face_alt.png'
+import memesData from "../memesData.js"
 
 export default function Generator(){
+
+    const [memeImage, setMemeImage] = React.useState("")
+    
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)        
+    }
+
 
     return(
         <div className="generator">
@@ -9,8 +19,8 @@ export default function Generator(){
                 <input type="text" className="generator-field field-1" placeholder="Shut up" />
                 <input type="text" className="generator-field field-2" placeholder="and take my money" />
             </div>
-            <button className="generator-btn">Get a new image meme</button>            
-            <img className="generator-meme" />
+            <button className="generator-btn" onClick={getMemeImage} >Get a new image meme 🖼</button>            
+            <img src={memeImage} className="generator-meme" />
         </div>
         
     )
